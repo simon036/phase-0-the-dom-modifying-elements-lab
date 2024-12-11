922a1ea2b88820533f03db8ea59bb8401b0022b9
Simon <ndungusimon564@gmail.com>
2024-12-11 22:58:46 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+//Set the text content of the new header
+newHeader.textContent = 'YOUR-NAME is the champion'; // Replace YOUR-NAME with your actual name
